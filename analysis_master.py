498d5142da1f292ff5f585318c758f0d8791f38e
dante <ruerue@gmail.com>
2019-01-12 13:06:15 -0600

    third commit

--- analysis_master.py
+++ analysis_master.py
@@ -1,8 +1,21 @@
 import pandas
+import json
+import requests
 
-
-import pandas
-
-
+import pandas as pd
 
 print("hello")
+
+res = requests.get("https://rebrickable.com/api/v3/lego/colors/?key=a33b0190e72500634fa56d408e217d12")
+
+print(res.headers)
+
+j = res.json()
+
+filename = 'temp.csv'
+df = pd.DataFrame(j['results'])
+
+print(df.head())
+print()
+
+df.to_csv(filename)
